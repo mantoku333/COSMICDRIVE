a81f9ef677c5bde0477970030ddec9cdbafca806
mantoku333 <mantoku714@gmail.com>
2026-01-14 20:52:31 +0900

    楽曲遷移サムネ

--- chumini/LoadingCanvas.h
+++ chumini/LoadingCanvas.h
@@ -4,6 +4,8 @@
 #include "SongInfo.h"
 #include "LoadingScene.h"
 
+#include "Texture.h"
+
 namespace app::test {
 
     class LoadingCanvas : public sf::ui::Canvas {
@@ -11,7 +13,7 @@ namespace app::test {
         void Begin() override;
         void Update(const sf::command::ICommand&);
 
-        // ƒV[ƒ“‚©‚çw¦‚ğó‚¯æ‚éŠÖ”
+        // ã‚·ãƒ¼ãƒ³ã‹ã‚‰æŒ‡ç¤ºã‚’å—ã‘å–ã‚‹é–¢æ•°
         void SetTargetScene(sf::SafePtr<sf::IScene> scene);
         void SetSongInfo(const SongInfo& info);
         void SetLoadingType(LoadingType type);
@@ -19,19 +21,20 @@ namespace app::test {
     private:
         sf::command::Command<> updateCommand;
 
-        // “Ç‚İ‚Ş‚×‚«Ÿ‚ÌƒV[ƒ“
+        // èª­ã¿è¾¼ã‚€ã¹ãæ¬¡ã®ã‚·ãƒ¼ãƒ³
         sf::SafePtr<sf::IScene> targetScene;
 
         sf::ui::TextImage* songTitleText = nullptr;
         sf::ui::TextImage* artistText = nullptr;
 
         sf::ui::Image* jacketImage = nullptr;
-        // UIƒp[ƒc
+        sf::Texture jacketTexture; // ã‚¸ãƒ£ã‚±ãƒƒãƒˆãƒ†ã‚¯ã‚¹ãƒãƒ£ã®å®Ÿä½“
+        // UIãƒ‘ãƒ¼ãƒ„
         sf::ui::TextImage* loadingText = nullptr;
         float timer = 0.0f;
         bool isLoadCompleted = false;
 
-		const float MIN_LOADING_TIME = 1.0f; // Å’á•\¦ŠÔ
+		const float MIN_LOADING_TIME = 1.0f; // æœ€ä½è¡¨ç¤ºæ™‚é–“
 
         LoadingType currentType = LoadingType::Common;
     };
