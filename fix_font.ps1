a8874376ab965318f829ba45bdb86db57daf202c
mantoku333 <mantoku714@gmail.com>
2026-03-05 08:52:57 +0900

    del

--- fix_font.ps1
+++
@@ -1,8 +0,0 @@
-$path = "chumini/IngameCanvas.cpp"
-$encoding = [System.Text.Encoding]::Default
-$content = [System.IO.File]::ReadAllText($path, $encoding)
-$find = 'L"851繧ｴ繝√き繧ｯ繝・ヨ"'
-$replace = 'L"851\x30B4\x30C1\x30AB\x30AF\x30C3\x30C8"'
-$content = $content.Replace($find, $replace)
-[System.IO.File]::WriteAllText($path, $content, $encoding)
-Write-Host "Replaced content."
--- fix_font_v2.ps1
+++
@@ -1,8 +0,0 @@
-$path = "chumini/IngameCanvas.cpp"
-$encoding = [System.Text.Encoding]::Default
-$content = [System.IO.File]::ReadAllText($path, $encoding)
-$findPattern = 'L"851[^"]*"'
-$replace = 'L"851\x30B4\x30C1\x30AB\x30AF\x30C3\x30C8"'
-$content = [regex]::Replace($content, $findPattern, $replace)
-[System.IO.File]::WriteAllText($path, $content, $encoding)
-Write-Host "Replaced content using Regex."
